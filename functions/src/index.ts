956dc6ce5338093e52ab037b5d87fbdb272b869a
Filippo Vigani <vigani.filippo@gmail.com>
2020-06-24 14:45:42 +0200

    Change telegram API library

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,24 +1,58 @@
 import * as functions from 'firebase-functions'
-import Telegraf, { Context } from 'telegraf'
+import TelegramBot = require('node-telegram-bot-api')
 
-const bot: Telegraf<Context> = new Telegraf(functions.config().bot.token)
+const token: string = functions.config().bot.token
 
-bot.telegram.setWebhook(functions.config().bot.url)
-.catch(error => {
-    console.log(error)
+const bot = new TelegramBot(token)
+
+bot.onText(new RegExp(/\/start/), (msg: TelegramBot.Message) => {
+    const options: TelegramBot.SendMessageOptions = {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    { text: "Create a new domain", callback_data: "create-domain" },
+                    { text: "Join an existing domain", callback_data: "join-domain" }
+                ]
+            ]
+        }
+    }
+
+    bot.sendMessage(
+        msg.chat.id,
+        `Welcome ${msg.from?.first_name}! To get started /create or /join a domain. Once you are in a domain you will be able to target groups to forward your messages to.`,
+        options
+    ).catch(err => console.log(err))
 })
 
-bot.command('/start', (ctx: Context) => ctx.reply('Hello').catch(error => {
-    console.log(error)
-}))
-bot.hears('yo', (ctx: Context) => ctx.reply('Yo').catch(error => {
-    console.log(error)
-}))
+bot.on("message", (message, metadata) => {
+    console.log(message)
+    bot.sendMessage(message.chat.id, 'I am alive!')
+    .catch(err => console.log(err))
+})
+
+exports.webhook = functions.region('europe-west3').https.onRequest((req, res) => {
+    const url: string = functions.config().bot.url
+    try {
+        const success = bot.setWebHook(url)
+        if (success)
+            res.send(`Webhook set to ${url}`)
+        else
+            res.send(`Failed setting webhook to ${url}`)
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(`Failed setting webhook to ${url}`)
+    }
+})
 
 exports.bot = functions.region('europe-west3').https.onRequest((req, res) => {
-    bot.handleUpdate(req.body, res)
-    .then( rv => !rv && res.sendStatus(200))
-    .catch(error => {
+    try {
+        console.log("REQUEST:")
+        console.log(req.body)
+        console.log("END_REQUEST")
+        //bot.processUpdate(req.body)
+        return res.sendStatus(200)
+    } catch (error) {
         console.log(error)
-    })
+        return res.status(500).send("Something went wrong")
+    }
 })
